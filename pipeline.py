5da51affb383dcd83196929ce177fe365218d138
Christopher Foo <chris.foo@gmail.com>
2014-12-19 17:09:15 -0500

    Add readme instructions, update tracker url to production

--- pipeline.py
+++ pipeline.py
@@ -60,7 +60,7 @@ if not WGET_LUA:
 VERSION = "20141219.01"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'roon'
-TRACKER_HOST = 'localhost:9080'
+TRACKER_HOST = 'tracker.archiveteam.org'
 
 
 ###########################################################################
